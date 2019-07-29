d482ad493f25f28e7d3590bbe2bfa88463b952c9
Savannah C <sgcorrell@gmail.com>
2019-07-29 15:37:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,19 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+  vaidates is_clickbait?
 end
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    errors.add(:title, "must be clickbait")
+    end
+  end
