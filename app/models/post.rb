faefef2b3e40051e05fd7d35ac8755c9b32e556c
Savannah C <sgcorrell@gmail.com>
2019-07-29 15:33:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, 
+  validates :category, inclusion: { in: %w(fiction non-fiction),
 end
